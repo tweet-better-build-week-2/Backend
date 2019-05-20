694ebeafd43584d3d40e57fd7e595ec628b92d6a
chrisipanaque <chris.ipanaque@gmail.com>
2019-05-20 11:58:23 -0700

    TDD - return status 422 if no tweet provided

--- api/server.spec.js
+++ api/server.spec.js
@@ -3,17 +3,32 @@ const server = require('./server');
 
 describe('server endpoints', () => {
   describe('POST /api/sentiment-rate', () => {
-    it('should return a JSON object with data', async () => {
+    it('should return a JSON object with sentiment rate', async () => {
       const requestBody = {
         text: 'This is a sample tweet with good/bad sentiment'
       };
-      
+
       const response = await request(server)
         .post('/api/sentiment-rate')
         .send(requestBody);
 
       expect(response.body).toEqual({
         sentimentRate: 5
+      });
+    });
+
+    it('should return 422 if no tweet provided', async () => {
+      const requestBody = {
+        text: ''
+      };
+
+      const response = await request(server)
+        .post('/api/sentiment-rate')
+        .send(requestBody);
+
+      expect(response.status).toBe(422);
+      expect(response.body).toEqual({
+        message: 'Please provide a tweet you would like to be analyzed.'
       });
     });
   });
