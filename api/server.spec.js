7516f867dd2d8a6cebf39baab74bd107f392bab8
chrisipanaque <chris.ipanaque@gmail.com>
2019-05-20 11:28:33 -0700

    POST - /api/sentiment-rate - TDD - requestBody

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,13 +1,20 @@
-const request = require('supertest')
-const server = require('./server')
+const request = require('supertest');
+const server = require('./server');
 
 describe('server endpoints', () => {
   describe('POST /api/sentiment-rate', () => {
     it('should return a JSON object with data', async () => {
-      const response = await request(server).post('/api/sentiment-rate')
+      const requestBody = {
+        text: 'This is a sample tweet with good/bad sentiment'
+      };
+      
+      const response = await request(server)
+        .post('/api/sentiment-rate')
+        .send(requestBody);
+
       expect(response.body).toEqual({
         sentimentRate: 5
-      })
-    })
-  })
-})
+      });
+    });
+  });
+});
